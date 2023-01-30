e4dbbcbc1259ff50c0c9ec0b04b8d878cd22235c
Nicolas <nicolasariel.enriquez@nttdata.com>
2023-01-30 17:53:20 -0300

    modificacion a peso argentino

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -1,6 +1,6 @@
 const monedas = [
     {id: 'USD', nombre: 'Dolar de Estados Unidos'},
-    {id: 'MXN', nombre: 'Peso Mexicano'},
+    {id: 'ARS', nombre: 'Peso Argentino'},
     {id: 'EUR', nombre: 'Euro'},
     {id: 'GBP', nombre: 'Libra Esterlina'},
 ]
